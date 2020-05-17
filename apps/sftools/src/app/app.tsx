83306524cd376252e34191012e852229690ad7cf
Austin Turner <paustint@gmail.com>
2020-05-17 10:09:18 -0600

    initial commit

---
+++ apps/sftools/src/app/app.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import { Message } from '@silverthorn/api-interfaces';
+
+export const App = () => {
+  const [m, setMessage] = useState<Message>({ message: '' });
+
+  useEffect(() => {
+    fetch('/api')
+      .then((r) => r.json())
+      .then(setMessage);
+  }, []);
+
+  return (
+    <>
+      <div style={{ textAlign: 'center' }}>
+        <h1>Welcome to sftools!</h1>
+        <img
+          width="450"
+          src="https://raw.githubusercontent.com/nrwl/nx/master/nx-logo.png"
+        />
+      </div>
+      <div>{m.message}</div>
+    </>
+  );
+};
+
+export default App;
