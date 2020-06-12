698b0a439b8bfdc5cd1211191d6a89148c3b22ce
= <paustint@gmail.com>
2020-06-12 16:37:10 -0500

    add build for storybook

--- postcss.config.js
+++ postcss.config.js
@@ -6,7 +6,17 @@ module.exports = {
       ? {
           '@fullhuman/postcss-purgecss': {
             content: ['./apps/landing/pages/**/*.*', './apps/landing/**/*.*'],
-            defaultExtractor: (content) => content.match(/[\w-/.:]+(?<!:)/g) || [],
+            // defaultExtractor: (content) => content.match(/[\w-/.:]+(?<!:)/g) || [],
+            // This is the function used to extract class names from your templates
+            defaultExtractor: (content) => {
+              // Capture as liberally as possible, including things like `h-(screen-1.5)`
+              const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
+
+              // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+              const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
+
+              return broadMatches.concat(innerMatches);
+            },
           },
         }
       : {}),
