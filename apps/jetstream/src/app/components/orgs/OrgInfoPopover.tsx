0f7454206b0fe48f12f16ad0378f775343fca9e1
Austin Turner <paustint@gmail.com>
2020-06-15 09:54:36 -0600

    Added login button to org dropdown

--- apps/jetstream/src/app/components/orgs/OrgInfoPopover.tsx
+++ apps/jetstream/src/app/components/orgs/OrgInfoPopover.tsx
@@ -39,33 +39,41 @@ export const OrgInfoPopover: FunctionComponent<OrgInfoPopoverProps> = ({ org }) 
         </header>
       }
       content={
-        <table className="slds-table slds-table_header-hidden">
-          <thead className="slds-assistive-text">
-            <tr className="slds-line-height_reset">
-              <th className="" scope="col">
-                <div className="slds-truncate" title="Org Property">
-                  Property
-                </div>
-              </th>
-              <th className="" scope="col">
-                <div className="slds-truncate" title="Value">
-                  Value
-                </div>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {getOrgProp(org, 'orgName', 'Org Name')}
-            {getOrgProp(org, 'organizationId', 'Org Id')}
-            {getOrgProp(org, 'orgInstanceName', 'Instance')}
-            {getOrgProp(org, 'instanceUrl')}
-            {getOrgProp(org, 'orgOrganizationType', 'Org Type')}
-            {getOrgProp(org, 'orgTrialExpirationDate', 'Trial Expiration')}
-            {getOrgProp(org, 'userId')}
-            {getOrgProp(org, 'username')}
-            {getOrgProp(org, 'email')}
-          </tbody>
-        </table>
+        <div>
+          <div className="slds-p-around_xx-small">
+            <button className="slds-button slds-button_neutral slds-button_stretch">
+              <Icon type="utility" icon="new_window" className="slds-button__icon slds-button__icon_left" omitContainer={true} />
+              Login
+            </button>
+          </div>
+          <table className="slds-table slds-table_header-hidden">
+            <thead className="slds-assistive-text">
+              <tr className="slds-line-height_reset">
+                <th className="" scope="col">
+                  <div className="slds-truncate" title="Org Property">
+                    Property
+                  </div>
+                </th>
+                <th className="" scope="col">
+                  <div className="slds-truncate" title="Value">
+                    Value
+                  </div>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {getOrgProp(org, 'orgName', 'Org Name')}
+              {getOrgProp(org, 'organizationId', 'Org Id')}
+              {getOrgProp(org, 'orgInstanceName', 'Instance')}
+              {getOrgProp(org, 'instanceUrl')}
+              {getOrgProp(org, 'orgOrganizationType', 'Org Type')}
+              {getOrgProp(org, 'orgTrialExpirationDate', 'Trial Expiration')}
+              {getOrgProp(org, 'userId')}
+              {getOrgProp(org, 'username')}
+              {getOrgProp(org, 'email')}
+            </tbody>
+          </table>
+        </div>
       }
     >
       <button className="slds-button slds-button_icon">
