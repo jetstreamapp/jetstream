fa31a45b9f8f17092335585c062a08ada279456d
Austin Turner <paustint@gmail.com>
2020-09-29 21:22:21 -0600

    upgraded nx

--- libs/ui/jest.config.js
+++ libs/ui/jest.config.js
@@ -6,4 +6,5 @@ module.exports = {
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
   coverageDirectory: '../../coverage/libs/ui',
+  globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
 };
