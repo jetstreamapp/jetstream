bc258e894c86f3263989db475c064126c07f6fcc
Austin Turner <paustint@gmail.com>
2020-10-04 19:11:30 -0700

    fixed gap css

--- apps/landing/components/early-access/FAQ.tsx
+++ apps/landing/components/early-access/FAQ.tsx
@@ -5,7 +5,7 @@ export const Faq = () => (
   <div className="max-w-screen-xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
     <h2 className="text-3xl leading-9 font-extrabold text-gray-900 text-center">Frequently asked questions</h2>
     <div className="mt-12">
-      <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:grid-rows-2 md:col-gap-8 md:row-gap-12 lg:grid-cols-3">
+      <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:grid-rows-2 md:gap-x-8 md:gap-x-12 lg:grid-cols-3">
         <FaqItem
           title="How does Jetstream get access to my orgs data and metadata?"
           text="Jetstream uses a Salesforce Connected App to login without ever obtaining access to your Salesforce password."
