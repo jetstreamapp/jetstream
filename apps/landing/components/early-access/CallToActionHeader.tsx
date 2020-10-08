2457933adf741f8827c3a5c2d51e2c9ae2f7e3ec
Austin Turner <paustint@gmail.com>
2020-10-08 08:19:17 -0700

    landing page updates

--- apps/landing/components/early-access/CallToActionHeader.tsx
+++ apps/landing/components/early-access/CallToActionHeader.tsx
@@ -5,8 +5,7 @@ export const CallToActionHeader = () => (
     <div className="max-w-screen-xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
       <div className="text-center">
         <div className="mt-1 text-4xl leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none sm:tracking-tight lg:text-6xl">
-          Supercharge your administration
-          <div className="text-blue-600">of Salesforce.com</div>
+          Salesforce at the speed of thought
         </div>
         <p className="max-w-xl mx-auto mt-5 text-xl leading-7 text-gray-500">Get early access for free.</p>
       </div>
