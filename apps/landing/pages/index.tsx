f82d2414ffe915bf23c178ecf6c9e9ae97b565b6
Austin Turner <paustint@gmail.com>
2020-10-17 10:54:30 -0600

    re-add landing page

--- apps/landing/pages/index.tsx
+++ apps/landing/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
-// import LandingPage from '../components/early-access/LandingPage';
+import LandingPage from '../components/early-access/LandingPage';
 import Footer from '../components/Footer';
 import NavBar from '../components/NavBar';
 import favicon from '../assets/images/favicon.ico';
@@ -17,7 +17,7 @@ export const Index = () => {
         <div className="relative py-3 px-4 sm:px-6 lg:px-8">
           <NavBar />
         </div>
-        {/* <LandingPage /> */}
+        <LandingPage />
       </div>
 
       <Footer currPage="home" />
