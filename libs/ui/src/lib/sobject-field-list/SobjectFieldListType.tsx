5b189fcdc8aa2170b64f8d653d9e3a1cd046ca6d
Austin Turner <paustint@gmail.com>
2020-11-03 18:58:49 -0700

    Copy type tooltip info to clipboard on click #145
    resolves #145

--- libs/ui/src/lib/sobject-field-list/SobjectFieldListType.tsx
+++ libs/ui/src/lib/sobject-field-list/SobjectFieldListType.tsx
@@ -3,24 +3,38 @@ import { FieldWrapper } from '@jetstream/types';
 import Tooltip from '../widgets/Tooltip';
 import Badge from '../widgets/Badge';
 import Icon from '../widgets/Icon';
+import copyToClipboard from 'copy-to-clipboard';
 
 export interface SobjectFieldListTypeProps {
   field: FieldWrapper;
 }
 
+function copy(value: string) {
+  copyToClipboard(value);
+}
+
+const copyToClipboardMsg = (
+  <em className="slds-m-top_x-small">
+    <small>click to copy to clipboard</small>
+  </em>
+);
+
 function getContent(field: FieldWrapper) {
+  let copyToClipboardValue: string = undefined;
   if (field.metadata.type === 'picklist') {
     let tooltipContent: JSX.Element = undefined;
     if (Array.isArray(field.metadata.picklistValues) && field.metadata.picklistValues.length > 0) {
+      copyToClipboardValue = field.metadata.picklistValues?.map((picklist) => picklist.value).join('\n');
       tooltipContent = (
         <span>
           {field.metadata.picklistValues?.map((picklist) => (
             <div key={picklist.value}>{picklist.label}</div>
           ))}
+          {copyToClipboardMsg}
         </span>
       );
       return (
-        <Tooltip id={`${field.name}-type-tooltip`} content={tooltipContent}>
+        <Tooltip id={`${field.name}-type-tooltip`} content={tooltipContent} onClick={() => copy(copyToClipboardValue)}>
           <span className="slds-badge__icon slds-badge__icon_left slds-badge__icon_inverse">
             <Icon
               type="utility"
@@ -34,8 +48,9 @@ function getContent(field: FieldWrapper) {
       );
     }
   } else if (field.metadata.calculatedFormula) {
+    copyToClipboardValue = field.metadata.calculatedFormula;
     return (
-      <Tooltip id={`${field.name}-type-tooltip`} content={field.metadata.calculatedFormula}>
+      <Tooltip id={`${field.name}-type-tooltip`} content={field.metadata.calculatedFormula} onClick={() => copy(copyToClipboardValue)}>
         <span className="slds-badge__icon slds-badge__icon_left slds-badge__icon_inverse">
           <Icon
             type="utility"
@@ -45,6 +60,7 @@ function getContent(field: FieldWrapper) {
           />
         </span>
         {field.type}
+        {copyToClipboardMsg}
       </Tooltip>
     );
   } else {
