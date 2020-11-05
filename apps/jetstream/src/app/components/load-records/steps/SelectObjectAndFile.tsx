453d053c50f5bf1915b4466f01b4e14449a4b1fc
Austin Turner <paustint@gmail.com>
2020-11-04 21:35:03 -0700

    fixed imports

--- apps/jetstream/src/app/components/load-records/steps/SelectObjectAndFile.tsx
+++ apps/jetstream/src/app/components/load-records/steps/SelectObjectAndFile.tsx
@@ -3,13 +3,10 @@ import { css, jsx } from '@emotion/core';
 import { INPUT_ACCEPT_FILETYPES } from '@jetstream/shared/constants';
 import { parseFile } from '@jetstream/shared/ui-utils';
 import { InputReadFileContent, InsertUpdateUpsertDelete, SalesforceOrgUi } from '@jetstream/types';
-import { ConnectedSobjectList, FileSelector, Grid, GridCol } from '@jetstream/ui';
+import { ConnectedSobjectList, FileSelector, Grid, GridCol, XlsxSheetSelectionModalPromise } from '@jetstream/ui';
 import { DescribeGlobalSObjectResult } from 'jsforce';
 import { FunctionComponent } from 'react';
 import Split from 'react-split';
-import { InsertUpdateUpsertDelete, SalesforceOrgUi, InputReadFileContent } from '@jetstream/types';
-import { ConnectedSobjectList, FileSelector, Grid, GridCol, Modal, XlsxSheetSelectionModalPromise } from '@jetstream/ui';
-import { INPUT_ACCEPT_FILETYPES } from '@jetstream/shared/constants';
 import LoadRecordsLoadTypeButtons from '../components/LoadRecordsLoadTypeButtons';
 import { FieldWithRelatedEntities } from '../load-records-types';
 
