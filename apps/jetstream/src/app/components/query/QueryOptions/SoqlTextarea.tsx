22f1a324f2b49fb444d02860c5fe64003e614362
Austin Turner <paustint@gmail.com>
2021-05-31 09:43:28 -0600

    soql is too tall (default height) #536
    reduced initial height of query soql
    added resize to query soql
    
    resolves #536

--- apps/jetstream/src/app/components/query/QueryOptions/SoqlTextarea.tsx
+++ apps/jetstream/src/app/components/query/QueryOptions/SoqlTextarea.tsx
@@ -1,35 +1,65 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/react';
 import { Grid, GridCol, Textarea } from '@jetstream/ui';
-import Editor from '@monaco-editor/react';
-import React, { Fragment, FunctionComponent } from 'react';
+import Editor, { OnMount } from '@monaco-editor/react';
+import { Fragment, FunctionComponent, memo, RefObject, useCallback, useRef } from 'react';
 import IncludeDeletedRecordsToggle from './IncludeDeletedRecords';
 import ManualSoql from './ManualSoql';
+import ResizeObserver from 'react-resize-detector';
+import type { editor } from 'monaco-editor';
 
 export interface SoqlTextareaProps {
   soql: string;
   isTooling: boolean;
 }
 
-export const SoqlTextarea: FunctionComponent<SoqlTextareaProps> = React.memo(({ soql, isTooling }) => {
+export const SoqlTextarea: FunctionComponent<SoqlTextareaProps> = memo(({ soql, isTooling }) => {
+  const divRef = useRef<HTMLDivElement>();
+  const editorRef = useRef<editor.IStandaloneCodeEditor>(null);
+
+  const handleEditorMount: OnMount = (currEditor, monaco) => {
+    editorRef.current = currEditor;
+    editorRef.current.layout();
+  };
+
+  const handleResize = useCallback(() => {
+    editorRef.current?.layout();
+  }, []);
+
   return (
     <Fragment>
       <Textarea id="soql-textarea" label="Generated SOQL">
-        <Editor
-          className="slds-border_top slds-border_right slds-border_bottom slds-border_left"
-          height="300px"
-          language="soql"
-          value={soql}
-          options={{
-            readOnly: true,
-            minimap: { enabled: false },
-            contextmenu: false,
-            lineNumbers: 'off',
-            glyphMargin: false,
-            folding: false,
-            selectionHighlight: false,
-            renderLineHighlight: 'none',
-            scrollBeyondLastLine: false,
-          }}
-        />
+        <ResizeObserver handleHeight targetRef={divRef} onResize={handleResize}>
+          {({ height }) => (
+            <div
+              ref={divRef}
+              className="slds-border_top slds-border_right slds-border_bottom slds-border_left"
+              css={css`
+                resize: vertical;
+                overflow: auto;
+                min-height: 150px;
+              `}
+            >
+              <Editor
+                height={height}
+                language="soql"
+                value={soql}
+                options={{
+                  readOnly: true,
+                  minimap: { enabled: false },
+                  contextmenu: false,
+                  lineNumbers: 'off',
+                  glyphMargin: false,
+                  folding: false,
+                  selectionHighlight: false,
+                  renderLineHighlight: 'none',
+                  scrollBeyondLastLine: false,
+                }}
+                onMount={handleEditorMount}
+              />
+            </div>
+          )}
+        </ResizeObserver>
       </Textarea>
       <Grid verticalAlign="center">
         <ManualSoql isTooling={isTooling} generatedSoql={soql} />
