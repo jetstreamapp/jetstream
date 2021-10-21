43f10a61cfee28b4c4c6f0d4fba9b33948ef6c4b
Austin Turner <paustint@gmail.com>
2021-10-20 20:05:54 -0600

    Added testimonial and learn to landing page

--- apps/landing/components/new/Learn.tsx
+++ apps/landing/components/new/Learn.tsx
@@ -6,60 +6,59 @@ import React from 'react';
 const blogPosts = [
   {
     id: 1,
-    title: 'Boost your conversion rate',
-    href: '#',
-    date: 'Mar 16, 2020',
-    datetime: '2020-03-16',
-    category: { name: 'Article', href: '#' },
-    imageUrl:
-      'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+    title: `Using Jetstream's advanced query builder`,
+    href: 'https://docs.getjetstream.app/query',
+    // date: 'Mar 16, 2020',
+    // datetime: '2020-03-16',
+    category: { name: 'Documentation' },
+    // imageUrl:
+    //   'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
     preview:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.',
-    author: {
-      name: 'Roel Aufderehar',
-      imageUrl:
-        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      href: '#',
-    },
-    readingLength: '6 min',
+      'Not only is the query builder great for viewing record data, but is also a great place to view your objects and fields along with a ton of useful information like field API names, picklist values, field dependencies and more.',
+    // author: {
+    //   name: 'Roel Aufderehar',
+    //   imageUrl:
+    //     'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    //   href: '#',
+    // },
+    // readingLength: '6 min',
   },
   {
     id: 2,
-    title: 'How to use search engine optimization to drive sales',
-    href: '#',
-    date: 'Mar 10, 2020',
-    datetime: '2020-03-10',
-    category: { name: 'Video', href: '#' },
-    imageUrl:
-      'https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
-    preview:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit facilis asperiores porro quaerat doloribus, eveniet dolore. Adipisci tempora aut inventore optio animi., tempore temporibus quo laudantium.',
-    author: {
-      name: 'Brenna Goyette',
-      imageUrl:
-        'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      href: '#',
-    },
-    readingLength: '4 min',
+    title: `How to use Jetstream's data loader to update data in your org`,
+    href: 'https://docs.getjetstream.app/load',
+    // date: 'Mar 10, 2020',
+    // datetime: '2020-03-10',
+    category: { name: 'Documentation' },
+    // imageUrl:
+    // 'https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+    preview: `Jetstream's data loader provides a simple and powerful way to update your orgs record data. Jetstream supports multiple file formats and automatically maps the fields in your file with the fields in your org.`,
+    // author: {
+    //   name: 'Brenna Goyette',
+    //   imageUrl:
+    //     'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    //   href: '#',
+    // },
+    // readingLength: '4 min',
   },
   {
     id: 3,
-    title: 'Improve your customer experience',
-    href: '#',
-    date: 'Feb 12, 2020',
-    datetime: '2020-02-12',
-    category: { name: 'Case Study', href: '#' },
-    imageUrl:
-      'https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+    title: `Make field level permission changes quickly`,
+    href: 'https://docs.getjetstream.app/permissions',
+    // date: 'Feb 12, 2020',
+    // datetime: '2020-02-12',
+    category: { name: 'Documentation' },
+    // imageUrl:
+    // 'https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
     preview:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum rerum voluptatem quo recusandae magni placeat saepe molestiae, sed excepturi cumque corporis perferendis hic.',
-    author: {
-      name: 'Daniela Metz',
-      imageUrl:
-        'https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      href: '#',
-    },
-    readingLength: '11 min',
+      'Jetstream makes it easy to view and modify object and field level security across many profiles and permission sets for multiple objects. Choose as many profiles, permission sets, and objects as you need to and update permissions quickly across all of them.',
+    // author: {
+    //   name: 'Daniela Metz',
+    //   imageUrl:
+    //     'https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    //   href: '#',
+    // },
+    // readingLength: '11 min',
   },
 ];
 
@@ -70,29 +69,28 @@ export const Learn = () => (
         <h2 className="text-base font-semibold tracking-wider text-cyan-600 uppercase">Learn</h2>
         <p className="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">Helpful Resources</p>
         <p className="mt-5 mx-auto max-w-prose text-xl text-gray-500">
-          Phasellus lorem quam molestie id quisque diam aenean nulla in. Accumsan in quis quis nunc, ullamcorper malesuada. Eleifend
-          condimentum id viverra nulla.
+          Check out the{' '}
+          <a className="underline" href="https://docs.getjetstream.app" target="_blank">
+            Jetstream documentation
+          </a>{' '}
+          for more information about using Jetstream.
         </p>
       </div>
       <div className="mt-12 mx-auto max-w-md px-4 grid gap-8 sm:max-w-lg sm:px-6 lg:px-8 lg:grid-cols-3 lg:max-w-7xl">
         {blogPosts.map((post) => (
           <div key={post.id} className="flex flex-col rounded-lg shadow-lg overflow-hidden">
-            <div className="flex-shrink-0">
-              <img className="h-48 w-full object-cover" src={post.imageUrl} alt="" />
-            </div>
+            {/* <div className="flex-shrink-0"> */}
+            {/* <img className="h-48 w-full object-cover" src={post.imageUrl} alt="" /> */}
+            {/* </div> */}
             <div className="flex-1 bg-white p-6 flex flex-col justify-between">
               <div className="flex-1">
-                <p className="text-sm font-medium text-cyan-600">
-                  <a href={post.category.href} className="hover:underline">
-                    {post.category.name}
-                  </a>
-                </p>
-                <a href={post.href} className="block mt-2">
+                <p className="text-sm font-medium text-cyan-600">{post.category.name}</p>
+                <a href={post.href} target="_blank" className="block mt-2">
                   <p className="text-xl font-semibold text-gray-900">{post.title}</p>
                   <p className="mt-3 text-base text-gray-500">{post.preview}</p>
                 </a>
               </div>
-              <div className="mt-6 flex items-center">
+              {/* <div className="mt-6 flex items-center">
                 <div className="flex-shrink-0">
                   <a href={post.author.href}>
                     <img className="h-10 w-10 rounded-full" src={post.author.imageUrl} alt={post.author.name} />
@@ -110,7 +108,7 @@ export const Learn = () => (
                     <span>{post.readingLength} read</span>
                   </div>
                 </div>
-              </div>
+              </div> */}
             </div>
           </div>
         ))}
