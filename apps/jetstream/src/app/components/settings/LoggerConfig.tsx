490891e213f2e4862289855a0576c6f8b2d2a440
Austin Turner <paustint@gmail.com>
2022-03-19 12:21:09 -0600

    Allow a way to turn on logs in production #1133
    Added setting to enable logging and print prior logs
    
    resolves #1133

---
+++ apps/jetstream/src/app/components/settings/LoggerConfig.tsx
@@ -0,0 +1,37 @@
+import { enableLogger, logBuffer, logger } from '@jetstream/shared/client-logger';
+import { CheckboxToggle, Grid } from '@jetstream/ui';
+import { FunctionComponent, useState } from 'react';
+
+export const LoggerConfig: FunctionComponent = () => {
+  const [isEnabled, setIsEnabled] = useState(logger.isEnabled);
+
+  function handleChange(value: boolean) {
+    setIsEnabled(value);
+    enableLogger(value);
+  }
+
+  function handleLogBuffer() {
+    if (logBuffer) {
+      logger.group('buffer');
+      logBuffer.forEach((item) => logger.log(item));
+      logger.groupEnd('buffer');
+    }
+  }
+
+  return (
+    <Grid verticalAlign="start">
+      <div>
+        <CheckboxToggle id="logging-toggle" checked={isEnabled} label="Logging" onChange={handleChange} />
+      </div>
+      {isEnabled && (
+        <div className="slds-m-left_medium">
+          <button className="slds-button slds-button_neutral" onClick={handleLogBuffer}>
+            Print Prior Logs
+          </button>
+        </div>
+      )}
+    </Grid>
+  );
+};
+
+export default LoggerConfig;
