0c8ec31d499d4144ca642cdd29a1aa4d59fa2651
Austin Turner <paustint@gmail.com>
2022-06-08 07:55:04 -0500

    upgraded nx-electron

--- electron-scripts/beforeBuild.js
+++ electron-scripts/beforeBuild.js
@@ -1,12 +1,13 @@
 exports.default = async function (context) {
-  // your custom code
-  console.warn('BEFORE BUILD!');
-  // TODO: add in full paths - update github ticket with workaround
-  context.targets.forEach((target) => {
-    target?.packager?.info?._configuration?.files?.forEach((file) => {
-      if (file.from) {
-        file.from = file.from.replace('${CWD}', process.cwd());
-      }
-    });
-  });
+  // @deprecated
+  // // your custom code
+  // console.warn('BEFORE BUILD!');
+  // // TODO: add in full paths - update github ticket with workaround
+  // context.targets.forEach((target) => {
+  //   target?.packager?.info?._configuration?.files?.forEach((file) => {
+  //     if (file.from) {
+  //       file.from = file.from.replace('${CWD}', process.cwd());
+  //     }
+  //   });
+  // });
 };
--- electron-scripts/beforePack.js
+++ electron-scripts/beforePack.js
@@ -7,15 +7,16 @@
  * @param {BeforePackContext} context
  */
 async function beforePack(context) {
-  // https://github.com/bennymeg/nx-electron/issues/169
-  console.warn('Monkey patching file paths');
-  context.targets.forEach((target) => {
-    target?.packager?.info?._configuration?.files?.forEach((file) => {
-      if (file.from) {
-        file.from = file.from.replace('${CWD}', process.cwd());
-      }
-    });
-  });
+  // @deprecated
+  // // https://github.com/bennymeg/nx-electron/issues/169
+  // console.warn('Monkey patching file paths');
+  // context.targets.forEach((target) => {
+  //   target?.packager?.info?._configuration?.files?.forEach((file) => {
+  //     if (file.from) {
+  //       file.from = file.from.replace('${CWD}', process.cwd());
+  //     }
+  //   });
+  // });
 }
 
 exports.default = beforePack;
