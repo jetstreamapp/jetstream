7778053e95055d83569d35a20798eee226736df6
Austin Turner <paustint@gmail.com>
2022-11-06 16:59:30 -0700

    Platform event react-table

--- apps/jetstream/src/app/components/platform-event-monitor/PlatformEventMonitorEvents.tsx
+++ apps/jetstream/src/app/components/platform-event-monitor/PlatformEventMonitorEvents.tsx
@@ -1,50 +1,75 @@
 import { ColDef, GetRowIdParams } from '@ag-grid-community/core';
 import { orderStringsBy } from '@jetstream/shared/utils';
-import { AutoFullHeightContainer, DataTable } from '@jetstream/ui';
+import { AutoFullHeightContainer, DataTable, DataTableNew } from '@jetstream/ui';
+import { ColumnWithFilter } from 'libs/ui/src/lib/data-table-new/data-table-types';
 import { FunctionComponent, useEffect, useState } from 'react';
 import { MessagesByChannel } from './usePlatformEvent';
+import groupBy from 'lodash/groupBy';
+import { FormatterProps, RowHeightArgs } from 'react-data-grid';
+import { css } from '@emotion/react';
 
-const columns: ColDef[] = [
+export const WrappedTextFormatter: FunctionComponent<FormatterProps<PlatformEventRow>> = ({ column, row }) => {
+  const value = row[column.key];
+  return (
+    <p
+      css={css`
+        white-space: pre-wrap;
+        line-height: normal;
+      `}
+    >
+      {value}
+    </p>
+  );
+};
+
+const columns: ColumnWithFilter<PlatformEventRow>[] = [
   {
-    headerName: 'Event',
-    colId: 'event',
-    field: 'event',
+    name: 'Event',
+    key: 'event',
     width: 230,
-    rowGroup: true,
-    hide: true,
-    lockVisible: true,
-    lockPosition: true,
-    tooltipField: 'event',
+    // rowGroup: true,
+    // hide: true,
+    // lockVisible: true,
+    // lockPosition: true,
+    // tooltipField: 'event',
+    frozen: true,
   },
   {
-    headerName: 'Payload',
-    colId: 'payload',
-    field: 'payload',
+    name: 'Payload',
+    key: 'payload',
     width: 450,
-    wrapText: true,
-    autoHeight: true,
+    // wrapText: true,
+    // autoHeight: true,
+    formatter: WrappedTextFormatter,
   },
   {
-    headerName: 'UUID',
-    colId: 'uuid',
-    field: 'uuid',
+    name: 'UUID',
+    key: 'uuid',
     width: 160,
-    tooltipField: 'uuid',
+    // tooltipField: 'uuid',
   },
   {
-    headerName: 'Replay Id',
-    colId: 'replayId',
-    field: 'replayId',
+    name: 'Replay Id',
+    key: 'replayId',
     width: 120,
-    tooltipField: 'replayId',
+    // tooltipField: 'replayId',
   },
 ];
 
-function getRowId({ data }: GetRowIdParams): string {
+const groupedRows = ['event'] as const;
+
+function getRowId(data: PlatformEventRow): string {
   return data.uuid;
 }
 
-interface PlatformEvenRow {
+function getRowHeight({ row, type }: RowHeightArgs<PlatformEventRow>) {
+  if (type === 'ROW') {
+    return 48;
+  }
+  return 24;
+}
+
+interface PlatformEventRow {
   event: string;
   payload: string;
   uuid: string;
@@ -56,13 +81,18 @@ export interface PlatformEventMonitorEventsProps {
 }
 
 export const PlatformEventMonitorEvents: FunctionComponent<PlatformEventMonitorEventsProps> = ({ messagesByChannel }) => {
-  const [rows, setRows] = useState<PlatformEvenRow[]>([]);
+  const [rows, setRows] = useState<PlatformEventRow[]>([]);
+  const [expandedGroupIds, setExpandedGroupIds] = useState(new Set<any>());
+
+  useEffect(() => {
+    setExpandedGroupIds(new Set(rows.map(({ event }) => event)));
+  }, [rows]);
 
   useEffect(() => {
     setRows(
       orderStringsBy(Object.keys(messagesByChannel)).flatMap((channel) =>
         messagesByChannel[channel].messages.map(
-          (message): PlatformEvenRow => ({
+          (message): PlatformEventRow => ({
             event: channel,
             payload: JSON.stringify(message.payload),
             uuid: message.event.EventUuid,
@@ -75,29 +105,34 @@ export const PlatformEventMonitorEvents: FunctionComponent<PlatformEventMonitorE
 
   return (
     <AutoFullHeightContainer fillHeight setHeightAttr delayForSecondTopCalc>
-      <DataTable
+      <DataTableNew
         columns={columns}
         data={rows}
-        defaultMenuTabs={['filterMenuTab', 'generalMenuTab']}
-        agGridProps={{
-          getRowId,
-          enableCellTextSelection: true,
-          enableRangeSelection: false,
-          autoGroupColumnDef: {
-            headerName: 'Event',
-            width: 200,
-            cellRenderer: 'agGroupCellRenderer',
-            filterParams: {
-              filters: [{ filter: 'agTextColumnFilter' }, { filter: 'agSetColumnFilter', filterParams: { showTooltips: true } }],
-            },
-            menuTabs: ['filterMenuTab'],
-            sortable: true,
-            resizable: true,
-            sort: 'asc',
-          },
-          showOpenedGroup: true,
-          groupDefaultExpanded: 1,
-        }}
+        getRowKey={getRowId}
+        groupBy={groupedRows}
+        rowGrouper={groupBy}
+        expandedGroupIds={expandedGroupIds}
+        onExpandedGroupIdsChange={(items) => setExpandedGroupIds(items)}
+        rowHeight={getRowHeight}
+        // defaultMenuTabs={['filterMenuTab', 'generalMenuTab']}
+        // agGridProps={{
+        //   getRowId,
+        //   enableCellTextSelection: true,
+        //   enableRangeSelection: false,
+        //   autoGroupColumnDef: {
+        //     name: 'Event',
+        //     key: 'agGroupCellRenderer',
+        //     filterParams: {
+        //       filters: [{ filter: 'agTextColumnFilter' }, { filter: 'agSetColumnFilter', filterParams: { showTooltips: true } }],
+        //     },
+        //     menuTabs: ['filterMenuTab'],
+        //     sortable: true,
+        //     resizable: true,
+        //     sort: 'asc',
+        //   },
+        //   showOpenedGroup: true,
+        //   groupDefaultExpanded: 1,
+        // }}
       />
     </AutoFullHeightContainer>
   );
