5c530d2fc891b6f43c9fb82bfe6608a890f52b55
Austin Turner <paustint@gmail.com>
2023-01-07 22:05:20 -0700

    Added excel generation methods

--- apps/jetstream/src/app/components/excelize/Excelize.tsx
+++ apps/jetstream/src/app/components/excelize/Excelize.tsx
@@ -19,31 +19,36 @@ export const Excelize: FunctionComponent<ExcelizeProps> = () => {
   const createSpreadsheet = useCallback(async () => {
     try {
       const { queryResults } = await query(
+        //     selectedOrg,
+        //     `SELECT Id, Name, AccountNumber, AccountSource, Active__c, AnnualRevenue,
+        //   api_name_mismatch__c, autonum__c, BillingAddress, BillingCity, BillingCountry,
+        //   BillingGeocodeAccuracy, BillingLatitude, BillingLongitude, BillingPostalCode, BillingState,
+        //   BillingStreet, ChannelProgramLevelName, ChannelProgramName, checkbox__c, CleanStatus,
+        //   CreatedById, CreatedDate, Currency_Field__c, CurrencyIsoCode, CustomerPriority__c,
+        //   DandbCompanyId, date__c, Description, DunsNumber, email__c, encrypted_text__c, Fax,
+        //   formula_number__c, Geolocation__c, Geolocation__Latitude__s, Geolocation__Longitude__s, Industry,
+        //   IsCustomerPortal, IsDeleted, IsPartner, Jigsaw, JigsawCompanyId, LastActivityDate,
+        //   LastModifiedById, LastModifiedDate, LastReferencedDate, LastViewedDate, MasterRecordId,
+        //   My_Ext_Id__c, my_perm_test_export_2__c, NaicsCode, NaicsDesc, number__c, NumberOfEmployees,
+        //   NumberofLocations__c, OperatingHoursId, OwnerId, Ownership, ParentId, percent__c, Phone, phone__c,
+        //   PhotoUrl, Rating, rich_text__c, roll_up__c, SBQQ__AssetQuantitiesCombined__c,
+        //   SBQQ__ContractCoTermination__c, SBQQ__CoTermedContractsCombined__c, SBQQ__CoTerminationEvent__c,
+        //   SBQQ__DefaultOpportunity__c, SBQQ__IgnoreParentContractedPrices__c, SBQQ__PreserveBundle__c,
+        //   SBQQ__PriceHoldEnd__c, SBQQ__RenewalModel__c, SBQQ__RenewalPricingMethod__c, SBQQ__TaxExempt__c,
+        //   ShippingAddress, ShippingCity, ShippingCountry, ShippingGeocodeAccuracy, ShippingLatitude,
+        //   ShippingLongitude, ShippingPostalCode, ShippingState, ShippingStreet, Sic, SicDesc, Site, SLA__c,
+        //   SLAExpirationDate__c, SLASerialNumber__c, SystemModstamp, Test123__c, Test_Field__c, textarea__c,
+        //   textarea_long__c, TickerSymbol, time__c, Tradestyle, Type, UpsellOpportunity__c, url__c, Website,
+        //   YearStarted
+        // FROM Account`
+        //   );
         selectedOrg,
-        `SELECT Id, Name, AccountNumber, AccountSource, Active__c, AnnualRevenue,
-      api_name_mismatch__c, autonum__c, BillingAddress, BillingCity, BillingCountry,
-      BillingGeocodeAccuracy, BillingLatitude, BillingLongitude, BillingPostalCode, BillingState,
-      BillingStreet, ChannelProgramLevelName, ChannelProgramName, checkbox__c, CleanStatus,
-      CreatedById, CreatedDate, Currency_Field__c, CurrencyIsoCode, CustomerPriority__c,
-      DandbCompanyId, date__c, Description, DunsNumber, email__c, encrypted_text__c, Fax,
-      formula_number__c, Geolocation__c, Geolocation__Latitude__s, Geolocation__Longitude__s, Industry,
-      IsCustomerPortal, IsDeleted, IsPartner, Jigsaw, JigsawCompanyId, LastActivityDate,
-      LastModifiedById, LastModifiedDate, LastReferencedDate, LastViewedDate, MasterRecordId,
-      My_Ext_Id__c, my_perm_test_export_2__c, NaicsCode, NaicsDesc, number__c, NumberOfEmployees,
-      NumberofLocations__c, OperatingHoursId, OwnerId, Ownership, ParentId, percent__c, Phone, phone__c,
-      PhotoUrl, Rating, rich_text__c, roll_up__c, SBQQ__AssetQuantitiesCombined__c,
-      SBQQ__ContractCoTermination__c, SBQQ__CoTermedContractsCombined__c, SBQQ__CoTerminationEvent__c,
-      SBQQ__DefaultOpportunity__c, SBQQ__IgnoreParentContractedPrices__c, SBQQ__PreserveBundle__c,
-      SBQQ__PriceHoldEnd__c, SBQQ__RenewalModel__c, SBQQ__RenewalPricingMethod__c, SBQQ__TaxExempt__c,
-      ShippingAddress, ShippingCity, ShippingCountry, ShippingGeocodeAccuracy, ShippingLatitude,
-      ShippingLongitude, ShippingPostalCode, ShippingState, ShippingStreet, Sic, SicDesc, Site, SLA__c,
-      SLAExpirationDate__c, SLASerialNumber__c, SystemModstamp, Test123__c, Test_Field__c, textarea__c,
-      textarea_long__c, TickerSymbol, time__c, Tradestyle, Type, UpsellOpportunity__c, url__c, Website,
-      YearStarted
-    FROM Account`
+        `SELECT Id, Name, BillingAddress FROM Account LIMIT 1 OFFSET 3`
       );
 
-      const results = await generateExcelWorksheet(queryResults.records);
+      const results = await generateExcelWorksheet(queryResults.records, {
+        header: Object.keys(queryResults.records[0]).filter((key) => key !== 'attributes'),
+      });
       await saveFile(results, 'test.xlsx', MIME_TYPES.XLSX_OPEN_OFFICE);
     } catch (ex) {
       logger.warn(ex);
--- libs/shared/ui-utils/src/lib/shared-ui-excel.ts
+++ libs/shared/ui-utils/src/lib/shared-ui-excel.ts
@@ -1,6 +1,6 @@
 import { logger } from '@jetstream/shared/client-logger';
 import type { NewFile, OpenReader } from 'excelize-wasm';
-import { clamp } from 'lodash';
+import { clamp, isBoolean, isNumber, isString } from 'lodash';
 
 type Excelize = {
   NewFile: typeof NewFile;
@@ -19,22 +19,100 @@ async function getExcelize() {
   return _excelize;
 }
 
+/**
+ * Generate single excel worksheet from data
+ * If header is not provided, it will be generated from keys of the first row of data
+ *
+ * @param data
+ * @param options
+ * @returns
+ */
 export async function generateExcelWorksheet(
   data: any[],
   options: {
     header?: string[];
     sheetName?: string;
     boldHeader?: boolean;
+    autoFilter?: boolean;
     autoSizeColumns?: boolean;
     freezeTopRow?: boolean;
   } = {}
 ) {
-  const { header = Object.keys(data[0]), sheetName = 'Records', boldHeader = true, autoSizeColumns = true, freezeTopRow = true } = options;
+  const { header = Object.keys(data[0]), sheetName = 'Records', ...restOptions } = options;
   // TODO: fallback to normal XLSX if this fails
   const excelize = await getExcelize();
 
   const spreadsheet = excelize.NewFile();
-  spreadsheet.SetSheetName('Sheet1', sheetName);
+  spreadsheet.NewSheet(sheetName);
+  spreadsheet.DeleteSheet('Sheet1');
+
+  addDataToWorksheet(spreadsheet, data, header, sheetName, restOptions);
+
+  const buffer = spreadsheet.WriteToBuffer();
+  if (!(buffer instanceof Uint8Array)) {
+    logger.warn('Error generating file', buffer);
+    throw new Error('Error generating file');
+  }
+
+  return buffer;
+}
+
+/**
+ * Generate excel worksheet from data. Each key in data will be a separate worksheet
+ * If headers is not provided, it will be generated from keys of the first row of data
+ * Headers should have same keys as data
+ *
+ * @param data
+ * @param options
+ * @returns
+ */
+export async function generateExcelWorksheets(
+  data: Record<string, any[]>,
+  options: {
+    headers?: Record<string, string[]>;
+    boldHeader?: boolean;
+    autoFilter?: boolean;
+    autoSizeColumns?: boolean;
+    freezeTopRow?: boolean;
+  } = {}
+) {
+  const { headers, ...restOptions } = options;
+  // TODO: fallback to normal XLSX if this fails
+  const excelize = await getExcelize();
+
+  const spreadsheet = excelize.NewFile();
+
+  for (const [sheetName, sheetData] of Object.entries(data)) {
+    spreadsheet.NewSheet(sheetName);
+    const header = headers[sheetName] || Object.keys(sheetData[0]);
+    addDataToWorksheet(spreadsheet, sheetData, header, sheetName, restOptions);
+  }
+
+  // delete default sheet
+  spreadsheet.DeleteSheet('Sheet1');
+
+  const buffer = spreadsheet.WriteToBuffer();
+  if (!(buffer instanceof Uint8Array)) {
+    logger.warn('Error generating file', buffer);
+    throw new Error('Error generating file');
+  }
+
+  return buffer;
+}
+
+export function addDataToWorksheet(
+  spreadsheet: NewFile,
+  data: any[],
+  header: string[],
+  sheetName: string,
+  options: {
+    boldHeader?: boolean; // default true
+    autoFilter?: boolean; // default true
+    autoSizeColumns?: boolean; // default true
+    freezeTopRow?: boolean; // default true
+  } = {}
+) {
+  const { boldHeader = true, autoSizeColumns = true, autoFilter = true, freezeTopRow = true } = options;
 
   const columnAddresses = getColumnAddresses(header.length);
   const columnContentMaxWidths: number[] = [];
@@ -42,46 +120,99 @@ export async function generateExcelWorksheet(
   // Add header row
   for (let i = 0; i < header.length; i++) {
     const value = header[i];
-    columnContentMaxWidths[i] = Math.max(columnContentMaxWidths[i] || 0, value.length || 0);
+    columnContentMaxWidths[i] = (value.length || 10) * (boldHeader ? 1.2 : 1);
     spreadsheet.SetCellValue(sheetName, `${columnAddresses[i]}1`, value);
   }
+
   // Add all other rows
   for (let row = 0; row < data.length; row++) {
     for (let col = 0; col < header.length; col++) {
       const rowData = data[row];
       const cellAddress = `${columnAddresses[col]}${row + 2}`;
-      let cellValue = rowData[header[col]] ?? ''; // TODO: confirm this value is "boolean | number | string" (date?)
-      columnContentMaxWidths[col] = Math.max(columnContentMaxWidths[col] || 0, cellValue.length || 0);
-      // maybe for dates we need to add a cell style?
-      const { error } = spreadsheet.SetCellValue(sheetName, cellAddress, cellValue);
-      if (error) {
-        logger.warn('Error setting cell value - falling back to string', error);
+
+      let cellValue = rowData[header[col]] ?? '';
+
+      if (cellValue && typeof cellValue === 'object') {
         cellValue = JSON.stringify(cellValue);
+      }
+
+      let setValueError: any;
+      if (isString(cellValue)) {
+        const { error } = spreadsheet.SetCellStr(sheetName, cellAddress, cellValue.substring(0, 32767));
+        setValueError = error;
+        columnContentMaxWidths[col] = Math.max(columnContentMaxWidths[col] || 0, cellValue?.length || 0);
+      } else if (isBoolean(cellValue)) {
+        const { error } = spreadsheet.SetCellBool(sheetName, cellAddress, cellValue);
+        setValueError = error;
+        columnContentMaxWidths[col] = Math.max(columnContentMaxWidths[col] || 0, 5);
+      } else if (isNumber(cellValue)) {
+        if (Number.isInteger(cellValue)) {
+          const { error } = spreadsheet.SetCellInt(sheetName, cellAddress, cellValue);
+          setValueError = error;
+        } else {
+          const { error } = spreadsheet.SetCellFloat(sheetName, cellAddress, cellValue, -1, 64);
+          setValueError = error;
+        }
+        columnContentMaxWidths[col] = Math.max(columnContentMaxWidths[col] || 0, cellValue.toString().length || 0);
+      } else {
+        const { error } = spreadsheet.SetCellValue(sheetName, cellAddress, cellValue);
+        setValueError = error;
+      }
+
+      // Fallback to JSON stringified value if we can't set the value
+      if (setValueError) {
+        logger.warn('Error setting cell value - falling back to string', setValueError);
+        cellValue = JSON.stringify(cellValue).substring(0, 32767);
         spreadsheet.SetCellValue(sheetName, cellAddress, cellValue);
         columnContentMaxWidths[col] = Math.max(columnContentMaxWidths[col] || 0, cellValue.length || 0);
       }
     }
   }
+  const firstCol = `${columnAddresses[0]}1`;
+  const finalCol = `${columnAddresses[header.length - 1]}1`;
+  const finalRowFirstCol = `A${data.length + 1}`;
+  const finalRowFinalCol = `${columnAddresses[header.length - 1]}${data.length + 1}`;
 
   if (boldHeader) {
     const { style, error } = spreadsheet.NewStyle(
       JSON.stringify({
         // Border: [{ Type: 'bottom', Color: 'ff0000', Style: 1 }],
-        Font: { Bold: true },
+        Font: { Bold: true, Size: 14 },
       })
     );
 
     if (!error) {
-      spreadsheet.SetCellStyle(sheetName, `${columnAddresses[0]}1`, `${columnAddresses[header.length - 1]}1`, style);
+      spreadsheet.SetCellStyle(sheetName, firstCol, finalCol, style);
     } else {
       logger.warn('Error generating style', error);
     }
   }
 
+  // TODO: should we detect columns with long text and wrap them?
+
+  if (autoFilter) {
+    spreadsheet.AutoFilter(sheetName, firstCol, finalRowFinalCol, '');
+  }
+
+  // FIXME: This does not seem to work correctly for some long cells
+  // Also, auto-wrap is set but does not work correctly in GSheets (shows applied, but does not wrap without unsetting and re-setting)
   if (autoSizeColumns) {
+    const { style } = spreadsheet.NewStyle(
+      JSON.stringify({
+        Alignment: { WrapText: true },
+      })
+    );
     for (let col = 0; col < header.length; col++) {
       const value = clamp(columnContentMaxWidths[col], 10, 200);
-      spreadsheet.SetColWidth(sheetName, columnAddresses[col], columnAddresses[col], value);
+
+      if (value > 100 && style) {
+        spreadsheet.SetColStyle(sheetName, `${columnAddresses[col]}:${columnAddresses[col]}`, style);
+      }
+
+      const { error } = spreadsheet.SetColWidth(sheetName, columnAddresses[col], columnAddresses[col], value);
+      if (error) {
+        logger.warn('Error setting column width', error);
+      }
     }
   }
 
@@ -100,14 +231,6 @@ export async function generateExcelWorksheet(
       })
     );
   }
-
-  const buffer = spreadsheet.WriteToBuffer();
-  if (!(buffer instanceof Uint8Array)) {
-    logger.warn('Error generating file', buffer);
-    throw new Error('Error generating file');
-  }
-
-  return buffer;
 }
 
 /**
