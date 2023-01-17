1db76554a4271f799e97079f9e36cd4dbd43d3dc
Austin Turner <paustint@gmail.com>
2023-01-16 17:09:41 -0700

    Log Invalid time value exception
    Not sure what the exact issue is
    added log statement to capture value that is problematic
    
    #17

--- libs/ui/src/lib/data-table/data-table-formatters.tsx
+++ libs/ui/src/lib/data-table/data-table-formatters.tsx
@@ -1,4 +1,5 @@
 import { DATE_FORMATS } from '@jetstream/shared/constants';
+import { logErrorToRollbar } from '@jetstream/shared/ui-utils';
 import formatDate from 'date-fns/format';
 import parseDate from 'date-fns/parse';
 import parseISO from 'date-fns/parseISO';
@@ -10,27 +11,47 @@ import isObject from 'lodash/isObject';
 import { SalesforceAddressField, SalesforceLocationField } from './data-table-types';
 
 export const dataTableDateFormatter = (dateOrDateTime: Date | string | null | undefined): string => {
-  if (!dateOrDateTime) {
-    return null;
-  } else if (isDate(dateOrDateTime)) {
-    return formatDate(dateOrDateTime as Date, DATE_FORMATS.YYYY_MM_DD_HH_mm_ss_a);
-  } else if (dateOrDateTime.length === 28) {
-    return formatDate(parseISO(dateOrDateTime), DATE_FORMATS.YYYY_MM_DD_HH_mm_ss_a);
-  } else if (dateOrDateTime.length === 10) {
-    return formatDate(startOfDay(parseISO(dateOrDateTime)), DATE_FORMATS.yyyy_MM_dd);
-  } else {
-    return dateOrDateTime;
+  try {
+    if (!dateOrDateTime) {
+      return null;
+    } else if (isDate(dateOrDateTime)) {
+      return formatDate(dateOrDateTime as Date, DATE_FORMATS.YYYY_MM_DD_HH_mm_ss_a);
+    } else if (dateOrDateTime.length === 28) {
+      return formatDate(parseISO(dateOrDateTime), DATE_FORMATS.YYYY_MM_DD_HH_mm_ss_a);
+    } else if (dateOrDateTime.length === 10) {
+      return formatDate(startOfDay(parseISO(dateOrDateTime)), DATE_FORMATS.yyyy_MM_dd);
+    } else {
+      return dateOrDateTime;
+    }
+  } catch (ex) {
+    logErrorToRollbar(ex.message, {
+      stack: ex.stack,
+      place: 'dataTableDateFormatter',
+      type: 'Error formatting date',
+      inputValue: dateOrDateTime,
+    });
+    return String(dateOrDateTime || '');
   }
 };
 
 export const dataTableTimeFormatter = (value: string | null | undefined): string => {
-  const time: string = value;
-  if (!time) {
-    return null;
-  } else if (time.length === 13) {
-    return formatDate(parseDate(time, DATE_FORMATS.HH_mm_ss_ssss_z, new Date()), DATE_FORMATS.HH_MM_SS_a);
-  } else {
-    return time;
+  try {
+    const time: string = value;
+    if (!time) {
+      return null;
+    } else if (time.length === 13) {
+      return formatDate(parseDate(time, DATE_FORMATS.HH_mm_ss_ssss_z, new Date()), DATE_FORMATS.HH_MM_SS_a);
+    } else {
+      return time;
+    }
+  } catch (ex) {
+    logErrorToRollbar(ex.message, {
+      stack: ex.stack,
+      place: 'dataTableDateFormatter',
+      type: 'Error formatting time',
+      inputValue: value,
+    });
+    return String(value || '');
   }
 };
 
@@ -38,25 +59,55 @@ export const dataTableFileSizeFormatter = (sizeInBytes: string | number | null |
   if (isNil(sizeInBytes)) {
     return null;
   }
-  return fileSizeFormatter(sizeInBytes as any);
+  try {
+    return fileSizeFormatter(sizeInBytes as any);
+  } catch (ex) {
+    logErrorToRollbar(ex.message, {
+      stack: ex.stack,
+      place: 'dataTableDateFormatter',
+      type: 'error formatting file size',
+      inputValue: sizeInBytes,
+    });
+    return String(sizeInBytes || '');
+  }
 };
 
 const newLineRegex = /\\n/g;
 
 export const dataTableAddressValueFormatter = (value: any): string => {
-  if (!isObject(value)) {
-    return null;
+  try {
+    if (!isObject(value)) {
+      return null;
+    }
+    const address: SalesforceAddressField = value;
+    const street = (address.street || '').replace(newLineRegex, '');
+    const remainingParts = [address.city, address.state, address.postalCode, address.country].filter((part) => !!part).join(', ');
+    return [street, remainingParts].join('\n');
+  } catch (ex) {
+    logErrorToRollbar(ex.message, {
+      stack: ex.stack,
+      place: 'dataTableDateFormatter',
+      type: 'error formatting address',
+      inputValue: value,
+    });
+    return String(value || '');
   }
-  const address: SalesforceAddressField = value;
-  const street = (address.street || '').replace(newLineRegex, '');
-  const remainingParts = [address.city, address.state, address.postalCode, address.country].filter((part) => !!part).join(', ');
-  return [street, remainingParts].join('\n');
 };
 
 export const dataTableLocationFormatter = (value: SalesforceLocationField | null | undefined): string => {
-  if (!value || !isObject(value)) {
-    return null;
+  try {
+    if (!value || !isObject(value)) {
+      return null;
+    }
+    const location: SalesforceLocationField = value as SalesforceLocationField;
+    return `Latitude: ${location.latitude}°, Longitude: ${location.longitude}°`;
+  } catch (ex) {
+    logErrorToRollbar(ex.message, {
+      stack: ex.stack,
+      place: 'dataTableDateFormatter',
+      type: 'error formatting location',
+      inputValue: value,
+    });
+    return String(value || '');
   }
-  const location: SalesforceLocationField = value as SalesforceLocationField;
-  return `Latitude: ${location.latitude}°, Longitude: ${location.longitude}°`;
 };
