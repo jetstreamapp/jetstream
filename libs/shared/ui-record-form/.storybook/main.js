22b69bcecd8211f75dae50469b51b5f05a474ac8
Austin Turner <paustint@gmail.com>
2023-03-19 09:05:44 -0600

    Upgrade NX
    Upgrade NX
    
    Remove global storybook configuration

--- libs/shared/ui-record-form/.storybook/main.js
+++ libs/shared/ui-record-form/.storybook/main.js
@@ -1,16 +1,8 @@
-const rootMain = require('../../../../.storybook/main');
-
 module.exports = {
-  ...rootMain,
-
-  stories: [...rootMain.stories, '../src/lib/**/*.stories.mdx', '../src/lib/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-essentials', ...rootMain.addons, '@nrwl/react/plugins/storybook'],
+  core: { builder: 'webpack5' },
+  stories: ['../src/lib/**/*.stories.mdx', '../src/lib/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-essentials', '@nrwl/react/plugins/storybook'],
   webpackFinal: async (config, { configType }) => {
-    // apply any global webpack configs that might have been specified in .storybook/main.js
-    if (rootMain.webpackFinal) {
-      config = await rootMain.webpackFinal(config, { configType });
-    }
-
     // add your own webpack tweaks if needed
 
     return config;
