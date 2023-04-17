696899175d1a7351ef2dec7391bb70f8b46213f7
Joie Cosby <jc283108@gmail.com>
2023-04-16 18:59:42 -0700

    removing accidentl push

--- apps/jetstream/src/app/components/load-records/components/load-results/LoadRecordsResultsModal.tsx
+++ apps/jetstream/src/app/components/load-records/components/load-results/LoadRecordsResultsModal.tsx
@@ -1,16 +1,6 @@
 import { css } from '@emotion/react';
 import { useNonInitialEffect } from '@jetstream/shared/ui-utils';
-import {
-  AutoFullHeightContainer,
-  ColumnWithFilter,
-  CopyToClipboard,
-  DataTable,
-  Icon,
-  Modal,
-  Tooltip,
-  setColumnFromType,
-  Spinner,
-} from '@jetstream/ui';
+import { AutoFullHeightContainer, ColumnWithFilter, DataTable, Icon, Modal, setColumnFromType, Spinner } from '@jetstream/ui';
 import { FunctionComponent, useCallback, useEffect, useRef, useState } from 'react';
 import { RowHeightArgs } from 'react-data-grid';
 
@@ -63,15 +53,6 @@ export const LoadRecordsResultsModal: FunctionComponent<LoadRecordsResultsModalP
                       line-height: normal;
                     `}
                   >
-                    {row._errors && (
-                      <Tooltip content={row._errors}>
-                        <CopyToClipboard
-                          icon={{ type: 'utility', icon: 'error', description: 'load error' }}
-                          content={row._errors}
-                          className="slds-text-color_error slds-p-right_x-small"
-                        />
-                      </Tooltip>
-                    )}
                     {row?._errors}
                   </p>
                 )
