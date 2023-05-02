5d1f2652b7653f7732f9336cb01f309a161ace7e
Joie Cosby <jc283108@gmail.com>
2023-05-01 18:30:30 -0700

    Dynamically setting Tooltip content depending on if copied from child component.

--- libs/ui/src/lib/widgets/TooltipCopyToClipboard.tsx
+++ libs/ui/src/lib/widgets/TooltipCopyToClipboard.tsx
@@ -1,32 +1,36 @@
 import { Maybe } from '@jetstream/types';
-import { Icon, Tooltip } from '@jetstream/ui';
-import { FunctionComponent } from 'react';
+import { CopyToClipboard, Tooltip } from '@jetstream/ui';
+import { FunctionComponent, useEffect, useState } from 'react';
 
 export interface TooltipCopyToClipboardProps {
   content: Maybe<string | JSX.Element>;
 }
 
 export const TooltipCopyToClipboard: FunctionComponent<TooltipCopyToClipboardProps> = (content) => {
-  console.log('content', content.content);
-  console.log('content', content);
+  const [displayContent, setDisplayContent] = useState(content.content);
+  const [copiedState, setCopiedState] = useState(false);
+
+  const handleCopied = (data) => {
+    setCopiedState(data);
+  };
+
+  useEffect(() => {
+    if (!copiedState) {
+      setDisplayContent(content.content);
+    } else {
+      setDisplayContent('Copied to Clipboard');
+    }
+  }, [handleCopied]);
+
   return (
-    <Tooltip content={content.content}>
-      <Icon
-        type="utility"
-        icon="error"
-        description="Failed"
-        title="Failed"
-        className="slds-button__icon slds-text-color_error slds-p-right_x-small"
+    <Tooltip content={displayContent}>
+      <CopyToClipboard
+        icon={{ type: 'utility', icon: 'error', description: 'Click to copy to clipboard' }}
+        content="test"
+        className="slds-text-color_error slds-p-right_x-small"
+        copied={handleCopied}
       />
     </Tooltip>
   );
 };
 export default TooltipCopyToClipboard;
-
-{
-  /* <CopyToClipboard
-icon={{ type: 'utility', icon: 'error', description: 'Click to copy to clipboard' }}
-content="test"
-className="slds-text-color_error slds-p-right_x-small"
-/> */
-}
