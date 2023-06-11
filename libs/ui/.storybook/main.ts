e19bf4c8521e0be61edff5ea3256570e8376b8c8
Austin Turner <paustint@gmail.com>
2023-06-11 14:15:14 -0600

    fix storybook migration

--- libs/ui/.storybook/main.ts
+++ libs/ui/.storybook/main.ts
@@ -1,19 +1,19 @@
-module.exports = {
-  core: {
-    builder: 'webpack5',
-  },
+import type { StorybookConfig } from '@storybook/react-webpack5';
+
+const config: StorybookConfig = {
+  framework: '@storybook/react-webpack5',
   stories: [
     '../src/lib/**/*.stories.mdx',
     '../src/lib/**/*.stories.@(js|jsx|ts|tsx)',
     '../../shared/**/src/lib/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: ['@storybook/addon-essentials', '@nx/react/plugins/storybook'],
-  webpackFinal: async (config, { configType }) => {
-    // add your own webpack tweaks if needed
-
-    return config;
+  features: {
+    // storyStoreV7: false, // 👈 Opt out of on-demand story loading
   },
   docs: {
     autodocs: true,
   },
 };
+
+export default config;
