73213cbadb16e7ddc8866c22685c1d94871f3204
Austin Turner <paustint@gmail.com>
2024-03-23 08:14:06 -0600

    Fix Build Issue
    Axios upgrade caused issue with Electron axios Adapter (Which wasn't even actually used)

--- apps/jetstream/src/app/components/core/config.ts
+++ apps/jetstream/src/app/components/core/config.ts
@@ -1,21 +1,22 @@
-import { logger } from '@jetstream/shared/client-logger';
-import { initForElectron } from '@jetstream/shared/data';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
-import { environment } from '../../../environments/environment';
-import { axiosElectronAdapter } from './electron-axios-adapter';
-import * as jetstreamElectron from './electron-utils';
+import { BrowserRouter } from 'react-router-dom';
+// import { logger } from '@jetstream/shared/client-logger';
+// import { initForElectron } from '@jetstream/shared/data';
+// import { environment } from '../../../environments/environment';
+// import { axiosElectronAdapter } from './electron-axios-adapter';
+// import * as jetstreamElectron from './electron-utils';
+// import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 export const CONFIG = {
   Router: BrowserRouter,
   baseName: '/app',
 };
 
-if (environment.isElectron || window.electron?.isElectron) {
-  CONFIG.Router = HashRouter;
-  CONFIG.baseName = '/';
-  initForElectron(axiosElectronAdapter);
-  (async () => {
-    logger.log('Loaded electron');
-    jetstreamElectron.init();
-  })();
-}
+// if (environment.isElectron || window.electron?.isElectron) {
+//   CONFIG.Router = HashRouter;
+//   CONFIG.baseName = '/';
+//   initForElectron(axiosElectronAdapter);
+//   (async () => {
+//     logger.log('Loaded electron');
+//     jetstreamElectron.init();
+//   })();
+// }
