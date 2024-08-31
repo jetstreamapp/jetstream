572aceb6fc1fc8023e7012ea56570be6670f4083
Austin Turner <paustint@gmail.com>
2024-08-31 08:36:19 -0500

    Fix environment parsing

--- libs/api-config/src/lib/env-config.ts
+++ libs/api-config/src/lib/env-config.ts
@@ -37,10 +37,10 @@ const EXAMPLE_USER: UserProfileServer = {
   updatedAt: new Date().toISOString(),
 };
 
-const booleanSchema = z.union([z.string(), z.boolean()]).optional().transform(ensureBoolean);
+const booleanSchema = z.union([z.string(), z.boolean()]).nullish().transform(ensureBoolean);
 const numberSchema = z
   .union([z.string(), z.number()])
-  .optional()
+  .nullish()
   .transform((val) => {
     if (isNumber(val) || !val) {
       return val ?? null;
@@ -50,60 +50,52 @@ const numberSchema = z
 
 export const ENV = z
   .object({
-    LOG_LEVEL: z.enum(['trace', 'debug', 'info', 'warn', 'error', 'fatal', 'silent']).optional().default('debug'),
+    LOG_LEVEL: z.enum(['trace', 'debug', 'info', 'warn', 'error', 'fatal', 'silent']).nullish().default('debug'),
     IS_CI: booleanSchema,
     // LOCAL OVERRIDE
     EXAMPLE_USER_OVERRIDE: booleanSchema,
-    EXAMPLE_USER: z.record(z.any()).optional(),
+    EXAMPLE_USER: z.record(z.any()).nullish(),
     IS_LOCAL_DOCKER: booleanSchema,
     // SYSTEM
-    NODE_ENV: z.string().optional().default('production'),
-    ENVIRONMENT: z.string().optional().default('production'),
+    NODE_ENV: z.string().nullish().default('production'),
+    ENVIRONMENT: z.string().nullish().default('production'),
     PORT: numberSchema.default(3333),
-    GIT_VERSION: z.string().optional(),
-    ROLLBAR_SERVER_TOKEN: z.string().optional(),
+    GIT_VERSION: z.string().nullish(),
+    ROLLBAR_SERVER_TOKEN: z.string().nullish(),
     // JETSTREAM
     JETSTREAM_SERVER_DOMAIN: z.string().url(),
     JETSTREAM_SESSION_SECRET: z.string(),
     JETSTREAM_SERVER_URL: z.string().url(),
     JETSTREAM_POSTGRES_DBURI: z.string(),
     JETSTREAM_CLIENT_URL: z.string().url(),
-    JETSTREAM_WORKER_URL: z.string().url().optional(),
+    JETSTREAM_WORKER_URL: z.string().url().nullish(),
     PRISMA_DEBUG: booleanSchema,
-    COMETD_DEBUG: z.enum(['error', 'warn', 'info', 'debug']).optional(),
+    COMETD_DEBUG: z.enum(['error', 'warn', 'info', 'debug']).nullish(),
     // AUTH
     // TODO: optional for dev mode?
     CLERK_SECRET_KEY: z.string(),
     CLERK_PUBLISHABLE_KEY: z.string(),
     CLERK_JWT_KEY: z.string(),
     CLERK_WEBHOOK_SECRET: z.string(),
-    // AUTH0
-    AUTH0_DOMAIN: z.string().optional(),
-    /** use for M2M tokens - in DEV this is the same, but different in production */
-    AUTH0_M2M_DOMAIN: z.string().optional(),
-    AUTH0_CLIENT_ID: z.string().optional(),
-    AUTH0_MGMT_CLIENT_ID: z.string().optional(),
-    AUTH0_MGMT_CLIENT_SECRET: z.string().optional(),
-    AUTH0_CLIENT_SECRET: z.string().optional(),
     // MAILGUN
-    MAILGUN_API_KEY: z.string().optional(),
-    MAILGUN_PUBLIC_KEY: z.string().optional(),
-    MAILGUN_WEBHOOK_KEY: z.string().optional(),
+    MAILGUN_API_KEY: z.string().nullish(),
+    MAILGUN_PUBLIC_KEY: z.string().nullish(),
+    MAILGUN_WEBHOOK_KEY: z.string().nullish(),
     // SFDC
-    SFDC_API_VERSION: z.string().default('60.0'),
+    SFDC_API_VERSION: z.string().default('61.0'),
     SFDC_CONSUMER_SECRET: z.string(),
     SFDC_CONSUMER_KEY: z.string(),
     SFDC_CALLBACK_URL: z.string().url(),
     // GOOGLE
-    GOOGLE_APP_ID: z.string().optional(),
-    GOOGLE_API_KEY: z.string().optional(),
-    GOOGLE_CLIENT_ID: z.string().optional(),
+    GOOGLE_APP_ID: z.string().nullish(),
+    GOOGLE_API_KEY: z.string().nullish(),
+    GOOGLE_CLIENT_ID: z.string().nullish(),
     // GITHUB
-    GITHUB_TOKEN: z.string().optional(),
+    GITHUB_TOKEN: z.string().nullish(),
     // HONEYCOMB
     HONEYCOMB_ENABLED: booleanSchema,
-    HONEYCOMB_API_KEY: z.string().optional(),
-    AUTH_AUDIENCE: z.string().optional(),
+    HONEYCOMB_API_KEY: z.string().nullish(),
+    AUTH_AUDIENCE: z.string().nullish(),
   })
   .parse({
     ...process.env,
