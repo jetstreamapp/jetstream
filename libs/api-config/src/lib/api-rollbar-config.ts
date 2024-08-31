188cd495475992f81124e411f521055925cd75a3
Austin Turner <paustint@gmail.com>
2024-08-31 09:05:21 -0500

    Fix type issue

--- libs/api-config/src/lib/api-rollbar-config.ts
+++ libs/api-config/src/lib/api-rollbar-config.ts
@@ -2,9 +2,9 @@ import Rollbar from 'rollbar';
 import { ENV } from './env-config';
 
 export const rollbarServer = new Rollbar({
-  codeVersion: ENV.GIT_VERSION,
-  code_version: ENV.GIT_VERSION,
-  accessToken: ENV.ROLLBAR_SERVER_TOKEN,
+  codeVersion: ENV.GIT_VERSION ?? undefined,
+  code_version: ENV.GIT_VERSION ?? undefined,
+  accessToken: ENV.ROLLBAR_SERVER_TOKEN ?? undefined,
   environment: ENV.ENVIRONMENT,
   captureUncaught: true,
   captureUnhandledRejections: true,
--- libs/api-config/src/lib/email.config.ts
+++ libs/api-config/src/lib/email.config.ts
@@ -8,6 +8,6 @@ if (ENV.MAILGUN_API_KEY) {
   mailgun = new Mailgun(formData).client({
     username: 'api',
     key: ENV.MAILGUN_API_KEY,
-    public_key: ENV.MAILGUN_PUBLIC_KEY,
+    public_key: ENV.MAILGUN_PUBLIC_KEY ?? undefined,
   });
 }
