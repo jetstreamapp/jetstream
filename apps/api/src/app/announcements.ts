1e85a6cf8ff09b04b0039d384af98f176d521ed1
Austin Turner <paustint@gmail.com>
2024-11-16 13:00:50 -0700

    Fix downtime expires time

--- apps/api/src/app/announcements.ts
+++ apps/api/src/app/announcements.ts
@@ -12,7 +12,7 @@ export function getAnnouncements(): Announcement[] {
         content:
           'We will be upgrading our authentication system with an expected start time of {start} in your local timezone. During this time, you will not be able to log in or use Jetstream. We expect the upgrade to take less than one hour.',
         replacementDates: [{ key: '{start}', value: '2024-11-16T18:00:00.000Z' }],
-        expiresAt: '2024-11-16T20:00:00.000Z',
+        expiresAt: '2024-11-16T19:45:11.373Z',
         createdAt: '2024-11-15T15:00:00.000Z',
       },
     ].filter(({ expiresAt }) => new Date(expiresAt) > new Date());
