250584b243a071576e3d703e46efe6e2fd441fa2
Austin Turner <paustint@gmail.com>
2025-05-29 07:22:10 -0700

    Remove support for in-app email when app crashes
    resolves #1270

--- libs/api-types/src/lib/api-user.types.ts
+++
@@ -1,6 +0,0 @@
-import { z } from 'zod';
-
-export const EmailSupportRequestSchema = z.object({
-  emailBody: z.string().min(1),
-});
-export type EmailSupportRequest = z.infer<typeof EmailSupportRequestSchema>;
