9622dc35b65705eae8f175672778a0c4518fe223
Austin Turner <paustint@gmail.com>
2025-07-26 13:37:28 -0600

    Fix bulk update records from query results
    The callback data was stale and not being updated in poll results which caused the polling to fail since the job was never attached to the data. utilized jotai useCallback to ensure we have a stable callback function while getting data dynamically from store

--- libs/features/query/src/QueryResults/BulkUpdateFromQuery/BulkUpdateFromQueryModal.tsx
+++ libs/features/query/src/QueryResults/BulkUpdateFromQuery/BulkUpdateFromQueryModal.tsx
@@ -27,10 +27,9 @@ import {
   fetchRecordsWithRequiredFields,
   useDeployRecords,
 } from '@jetstream/ui-core';
-import { applicationCookieState } from '@jetstream/ui/app-state';
 import { Query } from '@jetstreamapp/soql-parser-js';
 import { useAtom } from 'jotai';
-import { atomWithReset, useResetAtom } from 'jotai/utils';
+import { atomWithReset, useAtomCallback, useResetAtom } from 'jotai/utils';
 import isNumber from 'lodash/isNumber';
 import { ChangeEvent, FunctionComponent, useCallback, useEffect, useMemo, useState } from 'react';
 import BulkUpdateFromQueryRecordSelection from './BulkUpdateFromQueryRecordSelection';
@@ -102,7 +101,7 @@ export const BulkUpdateFromQueryModal: FunctionComponent<BulkUpdateFromQueryModa
   const [deployResults, setDeployResults] = useAtom(deployResultsState);
   const [didDeploy, setDidDeploy] = useState(false);
   const resetDeployResults = useResetAtom(deployResultsState);
-  const [{ serverUrl }] = useAtom(applicationCookieState);
+
   const targetedRecordCount = useMemo(() => {
     if (downloadRecordsValue === RADIO_ALL_BROWSER || downloadRecordsValue === RADIO_ALL_SERVER) {
       return totalRecordCount;
@@ -112,22 +111,19 @@ export const BulkUpdateFromQueryModal: FunctionComponent<BulkUpdateFromQueryModa
     }
     return selectedRecords.length;
   }, [downloadRecordsValue, filteredRecords.length, selectedRecords.length, totalRecordCount]);
-  // this allows the pollResults to have a stable data source for updated data
-  const getDeploymentResults = useCallback(() => {
-    return [
-      {
-        deployResults,
-        sobject,
-      },
-    ];
-  }, [deployResults, sobject]);
 
-  const handleDeployResults = useCallback((sobject: string, deployResults: DeployResults, fatalError?: boolean) => {
-    setDeployResults(deployResults);
-    if (fatalError) {
-      setErrorMessage('An error occurred while processing your request. Please try again.');
-    }
-  }, []);
+  // this allows the pollResults to have a stable data source for updated data
+  const getDeploymentResults = useAtomCallback(useCallback((get) => [{ deployResults: get(deployResultsState), sobject }], [sobject]));
+
+  const handleDeployResults = useCallback(
+    (sobject: string, deployResults: DeployResults, fatalError?: boolean) => {
+      setDeployResults(deployResults);
+      if (fatalError) {
+        setErrorMessage('An error occurred while processing your request. Please try again.');
+      }
+    },
+    [setDeployResults]
+  );
 
   const { loadDataForProvidedRecords, pollResultsUntilDone } = useDeployRecords(selectedOrg, handleDeployResults, 'QUERY');
 
