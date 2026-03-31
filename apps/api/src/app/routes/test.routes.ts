b34e07e060b1e5bab28e5b78f1ed3f0f25274b56
Austin Turner <paustint@gmail.com>
2026-03-31 07:38:02 -0700

    feat: use jwt auth for e2e org
    Instead of logging in using Salesforce UI for all basic tests, use JWT instead of username/password

--- apps/api/src/app/routes/test.routes.ts
+++ apps/api/src/app/routes/test.routes.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { ENV, logger } from '@jetstream/api-config';
 import { ApiConnection, getApiRequestFactoryFn } from '@jetstream/salesforce-api';
-import { salesforceLoginUsernamePassword_UNSAFE } from '@jetstream/salesforce-oauth';
+import { salesforceLoginJwtBearer } from '@jetstream/salesforce-oauth';
 import express, { Router } from 'express';
 import { initConnectionFromOAuthResponse } from '../controllers/oauth.controller';
 import { NotAllowedError } from '../utils/error-handler';
@@ -12,9 +12,17 @@ const routes: express.Router = Router();
 routes.use((req, _, next) => {
   const E2E_LOGIN_URL = process.env.E2E_LOGIN_URL;
   const E2E_LOGIN_USERNAME = process.env.E2E_LOGIN_USERNAME;
-  const E2E_LOGIN_PASSWORD = process.env.E2E_LOGIN_PASSWORD;
+  const SFDC_CI_CONSUMER_KEY = process.env.SFDC_CI_CONSUMER_KEY;
+  const SFDC_CI_PRIVATE_KEY_BASE64 = process.env.SFDC_CI_PRIVATE_KEY_BASE64;
 
-  if (!E2E_LOGIN_URL || !E2E_LOGIN_USERNAME || !E2E_LOGIN_PASSWORD || !ENV.EXAMPLE_USER || req.hostname !== 'localhost') {
+  if (
+    !E2E_LOGIN_URL ||
+    !E2E_LOGIN_USERNAME ||
+    !SFDC_CI_PRIVATE_KEY_BASE64 ||
+    !SFDC_CI_CONSUMER_KEY ||
+    !ENV.EXAMPLE_USER ||
+    req.hostname !== 'localhost'
+  ) {
     return next(new NotAllowedError('Route not allowed in this environment'));
   }
   next();
@@ -22,24 +30,21 @@ routes.use((req, _, next) => {
 
 /**
  * Create an org for the integration user to use in testing
- * this avoids the need to perform oauth for integration test environment
+ * Uses JWT bearer flow to avoid interactive browser login
  */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 routes.post('/e2e-integration-org', async (_: express.Request, res: express.Response, next: express.NextFunction) => {
   const E2E_LOGIN_URL = process.env.E2E_LOGIN_URL!;
   const E2E_LOGIN_USERNAME = process.env.E2E_LOGIN_USERNAME!;
-  const E2E_LOGIN_PASSWORD = process.env.E2E_LOGIN_PASSWORD!;
+  const SFDC_CI_CONSUMER_KEY = process.env.SFDC_CI_CONSUMER_KEY!;
+  const privateKey = Buffer.from(process.env.SFDC_CI_PRIVATE_KEY_BASE64!, 'base64').toString('utf-8');
 
-  const { id, access_token, instance_url } = await salesforceLoginUsernamePassword_UNSAFE(
-    {
-      clientId: ENV.SFDC_CONSUMER_KEY,
-      clientSecret: ENV.SFDC_CONSUMER_SECRET,
-      redirectUri: ENV.SFDC_CALLBACK_URL,
-      loginUrl: E2E_LOGIN_URL,
-    },
-    E2E_LOGIN_USERNAME,
-    E2E_LOGIN_PASSWORD,
-  );
+  const { id, access_token, instance_url } = await salesforceLoginJwtBearer({
+    clientId: SFDC_CI_CONSUMER_KEY,
+    privateKey,
+    loginUrl: E2E_LOGIN_URL,
+    username: E2E_LOGIN_USERNAME,
+  });
   const [userId, organizationId] = new URL(id).pathname.split('/').reverse();
 
   logger.info({ organizationId, userId });
