ef714f4cb56c96cdfd2a9602767094d8e66db8d6
Austin Turner <paustint@gmail.com>
2026-06-20 19:06:50 -0700

    feat: add Salesforce release workflow and interactive release script

--- apps/jetstream-canvas/src/utils/api-client.ts
+++ apps/jetstream-canvas/src/utils/api-client.ts
@@ -1,8 +1,6 @@
 import { ApiConnection, getApiRequestFactoryFn } from '@jetstream/salesforce-api';
 import { logger } from '@jetstream/shared/client-logger';
-import type { SalesforceOrgUi, SObjectOrganization } from '@jetstream/types';
 import { isObjectLike } from 'lodash';
-import { OrgAndApiConnection } from './canvas.types';
 
 export const fetchFn: Parameters<typeof getApiRequestFactoryFn>[0] = (url, options) => {
   return new Promise<Response>((resolve, reject) => {
@@ -41,55 +39,4 @@ export function initApiClient(): ApiConnection {
     logger,
     enableLogging: false,
   });
-}
-
-export async function initApiClientAndOrg(): Promise<OrgAndApiConnection> {
-  const apiConnection = initApiClient();
-  const apiVersion = window.sr.context.environment.version.api;
-
-  // https://login.salesforce.com/id/00D6g000008KX1jEAG/0056g000004tCpaAAE
-  const [userId, organizationId] = (await apiConnection.org.discovery()).identity.split('/').reverse().slice(0, 2);
-
-  apiConnection.updateSessionInfo({ apiVersion, userId, organizationId });
-
-  const identity = await apiConnection.org.identity();
-
-  let companyInfoRecord: SObjectOrganization | undefined;
-
-  try {
-    const { queryResults } = await apiConnection.query.query<SObjectOrganization>(
-      `SELECT Id, Name, Country, OrganizationType, InstanceName, IsSandbox, LanguageLocaleKey, NamespacePrefix, TrialExpirationDate FROM Organization`,
-    );
-    if (queryResults.totalSize > 0) {
-      companyInfoRecord = queryResults.records[0];
-    }
-  } catch (ex) {
-    logger.warn('Error getting org info %o', ex);
-  }
-
-  const org: SalesforceOrgUi = {
-    uniqueId: `${identity.organization_id}-${identity.user_id}`,
-    label: identity.username,
-    filterText: identity.username,
-    accessToken: window.sr.client.oauthToken,
-    instanceUrl: window.sr.client.instanceUrl,
-    loginUrl: window.sr.client.instanceUrl,
-    userId: identity.user_id,
-    email: identity.email,
-    organizationId: identity.organization_id,
-    username: identity.username,
-    displayName: identity.display_name,
-    thumbnail: identity.photos?.thumbnail,
-    orgName: companyInfoRecord?.Name || 'Unknown Organization',
-    orgCountry: companyInfoRecord?.Country,
-    orgOrganizationType: companyInfoRecord?.OrganizationType,
-    orgInstanceName: companyInfoRecord?.InstanceName,
-    orgIsSandbox: companyInfoRecord?.IsSandbox,
-    orgLanguageLocaleKey: companyInfoRecord?.LanguageLocaleKey,
-    orgNamespacePrefix: companyInfoRecord?.NamespacePrefix,
-    orgTrialExpirationDate: companyInfoRecord?.TrialExpirationDate,
-  };
-
-  // return new ApiClient(sessionInfo, apiVersion, org);
-  return { org, apiConnection };
 }
